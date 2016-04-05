709e6ac9d39b1fd8469af7127378ddc25aad0dfd
AraHaan <seandhunt_7@yahoo.com>
2016-04-05 01:19:44 -0500

    TinyURL for Python 3.x Test Repo for now.

---
+++ setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup
+import re
+
+requirements = []
+with open('requirements.txt') as f:
+  requirements = f.read().splitlines()
+
+version = '0.1.1'
+
+if not version:
+    raise RuntimeError('version is not set')
+
+readme = 'TinyURL lib for Python 3.x.'
+
+setup(name='TinyURL',
+      author='Decorater',
+      url='None', #None for right now till I make a Repo for TinyURL for Python 3.x.
+      version=version,
+      packages=['TinyURL'],
+      license='MIT',
+      description='Core Module for DecoraterBot',
+      long_description=readme,
+      include_package_data=True,
+      install_requires=requirements,
+      classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.x',
+        'Topic :: Internet',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities',
+      ]
+)
