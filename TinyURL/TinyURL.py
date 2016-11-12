f321a1be6ae3152b557a024a8714885aa97cd11e
AraHaan <seandhunt_7@yahoo.com>
2016-11-12 01:17:23 -0600

    v0.1.6

--- TinyURL/TinyURL.py
+++ TinyURL/TinyURL.py
@@ -1,12 +1,37 @@
 # -*- coding: utf-8 -*-
+"""
+The MIT License (MIT)
+
+Copyright (c) 2015 Decorater
+
+Permission is hereby granted, free of charge, to any person obtaining a
+copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+DEALINGS IN THE SOFTWARE.
+"""
 import sys
-import urllib
+import urllib.request
+import urllib.parse
+from bs4 import BeautifulSoup
 import optparse
 from .errors import *
 
 API_CREATE = "http://tinyurl.com/api-create.php"
 # This is for setting a Alias in a url. This second one is needed (for if you want to customize the shortened url).
-API_CREATE2 = "http://tinyurl.com/create.php?source=indexpage&url={0}&submit=Make+TinyURL%21&alias={1}"
+API_CREATE2 = "http://tinyurl.com/create.php?"
 DEFAULT_DELIM = "\n"
 USAGE = """%prog [options] url [url url ...]
  
@@ -34,10 +59,20 @@ def create_one(url, alias=None):
         if url.startswith('http://') or url.startswith('https://') or url.startswith('ftp://'):
             if alias is not None:
                 if alias != '':
-                    url_code = API_CREATE2.format(url, alias)
-                    ret = urllib.request.urlopen(url_code, data=url_code).read()
-                    result = str(ret).replace("b", "").replace("'", "")
-                    return result
+                    payload = {
+                        'url': url,
+                        'submit': 'Make TinyURL!',
+                        'alias': alias
+                    }
+                    data = urllib.parse.urlencode(payload)
+                    full_url = API_CREATE2 + data
+                    ret = urllib.request.urlopen(full_url)
+                    soup = BeautifulSoup(ret, 'html.parser')
+                    check_error = soup.p.b.string
+                    if "The custom alias" in check_error:
+                        raise AliasUsed('The given Alias you have provided is already bwing used.')
+                    else:
+                        return soup.find_all('div', {'class': 'indent'})[1].b.string
                 else:
                     raise InvalidAlias('The given Alias cannot be \'empty\'.')
             else:
