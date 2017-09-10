e9fe0c0f37f71152f2e9d23cdd199ea22e6f87f4
AraHaan <seandhunt_7@yahoo.com>
2017-09-10 17:59:25 -0400

    Changed License stuff.

--- TinyURL.py
+++ TinyURL.py
@@ -2,7 +2,7 @@
 """
 The MIT License (MIT)
 
-Copyright (c) 2015-2017 Decorater
+Copyright (c) 2015-2017 IzunaDevs
 
 Permission is hereby granted, free of charge, to any person obtaining a
 copy of this software and associated documentation files (the "Software"),
